366b7de8b654e67cbf58f39956fa3c9dfba94fcf
omarsherif176 <68480294+omarsherif176@users.noreply.github.com>
2021-01-06 16:04:05 +0200

    Add files via upload

---
+++ New Phase 2/Components/XNOR.cpp
@@ -0,0 +1,47 @@
+#include "XNOR.h"
+
+XNOR::XNOR(const GraphicsInfo& r_GfxInfo, int r_FanOut) :Gate(2, r_FanOut)
+{
+	m_GfxInfo.x1 = r_GfxInfo.x1;
+	m_GfxInfo.y1 = r_GfxInfo.y1;
+	m_GfxInfo.x2 = r_GfxInfo.x2;
+	m_GfxInfo.y2 = r_GfxInfo.y2;
+}
+
+
+void XNOR::Operate()
+{
+	int n = (GetInputPinStatus(1) && GetInputPinStatus(2)) || ( (!GetInputPinStatus(1)) &&  (!GetInputPinStatus(2)) );
+	if (n==0)
+		m_OutputPin.setStatus(LOW);
+	else
+		m_OutputPin.setStatus(HIGH);
+}
+
+
+// Function Draw
+// Draws 2-input XNOR gate
+void XNOR:: Draw(Output* pOut, bool Selected)
+{
+	//Call output class and pass gate drawing info to it.
+	pOut->DrawXNOR(m_GfxInfo, Selected);
+}
+
+//returns status of outputpin
+int XNOR::GetOutPinStatus()
+{
+	return m_OutputPin.getStatus();
+}
+
+
+//returns status of Inputpin #n
+int XNOR::GetInputPinStatus(int n)
+{
+	return m_InputPins[n - 1].getStatus();	//n starts from 1 but array index starts from 0.
+}
+
+//Set status of an input pin ot HIGH or LOW
+void XNOR::setInputPinStatus(int n, STATUS s)
+{
+	m_InputPins[n - 1].setStatus(s);
+}
